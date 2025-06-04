52f7cac23eed413164b8b12ccbf041e85fb0e403
Alex St John <alexandros.georgakoudi@gmail.com>
2025-06-04 16:59:19 +0300

    Validated new http fetch service

--- internal/services/http_fetch.go
+++ internal/services/http_fetch.go
@@ -5,49 +5,63 @@ import (
   "io"
   "net/http"
   "os"
+  "path/filepath"
 
   "github.com/AlexSTJO/cli-flow/internal/structures"
 )
 
 type HttpFetchService struct{}
 
+func (s HttpFetchService) Name() string {
+  return "http_fetch"
+}
+
 func (s HttpFetchService) Run(step structures.Step) error {
   urlRaw, ok := step.Config["url"]
   if !ok {
-    return fmt.Errorf("[Error] Missing 'url' in config")
+    return fmt.Errorf(" Missing 'url' in config")
   }
 
   destRaw, ok := step.Config["destination"]
   if !ok {
-    return fmt.Errorf("[Error] Missing 'destination' in config")
+    return fmt.Errorf(" Missing 'destination' in config")
   }
 
   url, ok := urlRaw.(string)
   if !ok {
-    return fmt.Errorf("[Error] 'url' must be a string")
+    return fmt.Errorf(" 'url' must be a string")
   }
 
   dest, ok := destRaw.(string)
   if !ok {
-    return fmt.Errorf("[Error] 'dest' must be a string")
+    return fmt.Errorf(" 'dest' must be a string")
   }
 
   fmt.Printf("[Download] Fetching: %s\n", url)
 
   resp, err := http.Get(url)
   if err != nil {
-    return fmt.Errorf("[Error] Non-200 response: %d", resp.StatusCode)
+      return fmt.Errorf("failed to GET URL: %w", err)
+  }
+  defer resp.Body.Close()
+
+  if resp.StatusCode != 200 {
+      return fmt.Errorf("non-200 response: %d", resp.StatusCode)
+  }
+
+  if err := os.MkdirAll(filepath.Dir(dest), 0755); err != nil {
+    return fmt.Errorf("failed to create directories: %w", err)
   }
 
   outFile, err := os.Create(dest)
   if err != nil {
-    return fmt.Errorf("[Error] Failed to create file: %w", err)
+    return fmt.Errorf(" Failed to create file: %w", err)
   }
-  def outFile.Close()
+  defer outFile.Close()
 
   _, err = io.Copy(outFile, resp.Body)
   if err != nil {
-    return fmt.Errorf("[Error] Failed to write to file: %w", err)
+    return fmt.Errorf(" Failed to write to file: %w", err)
   }
 
   fmt.Printf("[Download] Saved to: %s\n", dest)
