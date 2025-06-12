93ebeb135fdf31870ed33969e8bd35d3697c810b
Alex St John <alexandros.georgakoudi@gmail.com>
2025-06-12 21:55:25 +0300

    Studied for saa exam today didnt do much, but smtp service registerd, not validated

--- internal/services/smtp_email.go
+++ internal/services/smtp_email.go
@@ -1,8 +1,13 @@
 package services
 
 import (
+  "os"
+  "net/smtp"
+  "fmt"
+
 
 	"github.com/AlexSTJO/cli-flow/internal/structures"
+  "github.com/AlexSTJO/cli-flow/internal/config"
 )
 
 type SmtpEmailService struct{}
@@ -12,10 +17,57 @@ func (s SmtpEmailService) Name() string {
 }
 
 func (s SmtpEmailService) ConfigSpec() []string {
-	return []string{"destination_email", "content", "port", "host"}
+	return []string{"destination_email", "subject", "body", "port", "host"}
 }
 
-func (s SmtpEmailService) Run(structures.Step) (error) {
-	return nil
+func (s SmtpEmailService) Run(step structures.Step) (Context, error) {
+  if err := config.HandleSmtpConfig(); err != nil{
+    return nil, fmt.Errorf("Error getting smtp config: %w", err)
+  }
+  from := os.Getenv("SMTPEmailAddress")
+  pwd := os.Getenv("SMTPEmailPassword")
+  host, ok := step.Config["host"].(string)
+  if !ok {
+    return nil, fmt.Errorf("host must be a string")
+  }
+  port, ok := step.Config["port"].(string)
+  if !ok {
+    return nil, fmt.Errorf("port must be a string")
+  }
 
+  subject, ok := step.Config["subject"].(string)
+  if !ok {
+    return nil, fmt.Errorf("subject must be a string")
+  }
+
+  body, ok := step.Config["body"].(string)
+  if !ok {
+    return nil, fmt.Errorf("body must be string")
+  }
+
+  destinationEmail, ok := step.Config["destination_email"].(string)
+  if !ok {
+    fmt.Errorf("Destination Email must be string")
+  }
+
+  auth := smtp.PlainAuth("", from, pwd, host)
+
+  msg := []byte(
+    "To: " + destinationEmail + "\r\n" +
+    "Subject: " + subject + "\r\n" +
+    "\r\n" + 
+    body + "\r\n")
+
+  
+  err := smtp.SendMail(host+":"+port, auth, from, []string{destinationEmail}, msg)
+  if err != nil{
+    return nil, fmt.Errorf("failed to send email: %w", err)
+  }
+
+  return Context{ "exit_code": 0, "status": "success"}, nil
+}
+
+
+func init(){
+  Registry["smtp_email"] = &SmtpEmailService{}
 }
